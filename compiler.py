3db1688d606266561927c796156a2b9e31ee410e
Lxs_ <112419013+Lxs06@users.noreply.github.com>
2024-02-22 18:37:48 +0000

    code finished

--- compiler.py
+++ compiler.py
@@ -43,6 +43,16 @@ lines = in_file.readlines()
 os.remove('output.txt')
 out_file = open('output.txt', "a")
 
+def error():
+    end_time = time.time
+    total_time = end_time - start_time
+    print("There was an error in the code at line: " + line_count)
+    print("After " + total_time + " seconds")
+    in_file.close
+    out_file.close
+    os.remove('output.txt')
+    exit()
+
 start_time = time.time
 for line in lines:
     line_count += 1
@@ -50,22 +60,21 @@ for line in lines:
     words = line.split(' ')
     for x in words:
         if x.isnumeric:
-            x = bin(x)
-            x = x.removeprefix("0b")
-            x = x.removeprefix("1b")
+            if x < 256:
+                x = bin(x)
+                if int(x) > 0:
+                    x = x.removeprefix("0b")
+                else:
+                    x = x.removeprefix("1b")
+            else: 
+                error()
         else:
             x.lower()
             if x in opcodes:
                 x = opcodes.get(x)
             else:
-                end_time = time.time
-                total_time = end_time - start_time
-                print("There was an error in the code at line: " + line_count)
-                print("After " + total_time + " seconds")
-                in_file.close
-                out_file.close
-                os.remove('output.txt')
-                exit()
+                error()
+                
         newline += x
     out_file.write(newline + '\n')
     print(newline)
