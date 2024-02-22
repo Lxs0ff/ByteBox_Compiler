020f698758dbbe2678aece32fff0c574043a8fdb
Lxs_ <112419013+Lxs06@users.noreply.github.com>
2024-02-22 18:19:54 -0500

    Update out_to_mcschem.py

--- out_to_mcschem.py
+++ out_to_mcschem.py
@@ -7,7 +7,39 @@ except:
     print("McShem library not found, importing library...")
     subprocess.run(["pip", "install", "mcschematic"]) 
 
+x = 0
+y = 0
+z = 0
+
 schem = mcschematic.MCSchematic()
+
+bin_file = open('output.txt', "r")
+bin_lines = bin_file.readlines()
+
+for line in lines:
+    line_count += 1
+    if line_count > 1:
+        if line_count == 17:
+            y = -1
+            x = 
+            z =
+        else:
+            += 4
+            
+            
+    if line_count > 32:
+        break
+    for bit in line:
+        y -= 2
+        if line_count == 1:
+            y = -1
+        if bit = 0:
+            schem.place()
+        else:
+            if bit = 1:
+                schem.place()
+            else:
+                schem.place()
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
