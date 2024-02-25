531ed1f308aec6ffdfbaf83a6d08486e0b381e72
Lxs_ <112419013+Lxs06@users.noreply.github.com>
2024-02-24 23:40:27 -0500

    fix out_to_schem

--- out_to_mcschem.py
+++ out_to_mcschem.py
@@ -10,30 +10,31 @@ except:
 x = 0
 y = 0
 z = 0
+line_count = 0
 schem = mcschematic.MCSchematic()
 
 bin_file = open('output.txt', "r")
 bin_lines = bin_file.readlines()
 
-for line in lines:
+for line in bin_lines:
     line_count += 1
     if line_count > 1:
         if line_count == 17:
             y = -1
-            x = 
-            z =
+            x = 0
+            z = 8
         else:
-            += 4
+            x += 4
     if line_count > 32:
         break
     for bit in line:
         y -= 2
         if line_count == 1:
             y = -1
-        if bit = 0:
+        if bit == 0:
             schem.place()
         else:
-            if bit = 1:
+            if bit == 1:
                 schem.setBlock( (x,y,z) , "minecraft:redstone_block")
             else:
                 schem.setBlock( (x,y,z) , "minecraft:air")
