1d76faf1b0aea73c39f64d3bdd459a80321b24ab
Lxs_ <112419013+Lxs06@users.noreply.github.com>
2024-02-26 13:43:30 +0000

    fix

--- out_to_mcschem.py
+++ out_to_mcschem.py
@@ -12,13 +12,16 @@ x = 0
 y = 0
 z = 0
 line_count = 0
+bit_count = 0
 schem = mcschematic.MCSchematic()
 
 bin_file = open('output.txt', "r")
 bin_lines = bin_file.readlines()
 
 for line in bin_lines:
+    bit_count = 0
     line_count += 1
+    print(str(line_count) + ": " + str(line))
     if line_count > 1:
         if line_count == 17:
             y = -1
@@ -26,20 +29,30 @@ for line in bin_lines:
             z = 8
         else:
             x += 4
-    if line_count > 32:
-        break
-    for bit in line:
-        y -= 2
         if line_count == 1:
             y = -1
-        if bit == 0:
-            schem.setBlock((x,y,z), "minecraft:air")
+    if line_count > 32:
+        print("readched max line count")
+        break
+    for bit in line:
+        bit_count += 1
+        if bit_count == 1:
+            y = -1
         else:
-            if bit == 1:
-                schem.setBlock( (x,y,z) , "minecraft:redstone_block")
+            y -= 2
+        if bit != "\n":
+            print("Pos(bit:" + str(bit_count) + "): (" + str(x) + "," + str(y) + "," + str(z) + ")")
+            print("Bit: "+str(bit))
+            if bit == "1":
+                schem.setBlock((x,y,z), "minecraft:redstone_block")
+                print("1 -> Redstone Block")
             else:
-                schem.setBlock( (x,y,z) , "minecraft:air")
+                schem.setBlock( (x,y,z), "minecraft:air")
+                print("0 -> Air Block")
+        else:
+            print("\nEnd of line \n")
 
+print("Conversion done...")
 now = datetime.now()
 current_time = now.strftime("%H_%M")
 schem.save(  "schematics", current_time + "_code", mcschematic.Version.JE_1_19_4)
